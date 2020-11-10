af878f90e55cb7dbdd1f08c410d01ff9090dc13b
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-11-10 14:58:20 -0600

    adapting jobscripts for frontera

--- jobscripts/ca1_centerlfp_test_exc_065_01.sh
+++ jobscripts/ca1_centerlfp_test_exc_065_01.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name="ca1_centerlfp_test_exc_065_01"
 #SBATCH --output="./jobscripts/ca1_centerlfp_test_exc_065_01.%N.o"
-#SBATCH --partition=compute
-#SBATCH --nodes 4
-#SBATCH --ntasks-per-node=12
-#SBATCH --export=ALL
-#SBATCH -t 0:30:00
+#SBATCH --partition=normal
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=56
+#SBATCH -p normal
+#SBATCH -t 2:00:00
 
-module load python
-module load mpi4py
 
 set -x
 
-ibrun -v x86_64/special -mpi ./jobscripts/ca1_centerlfp_test_exc_065_01_run.hoc
+mkdir -p ./results/ca1_centerlfp_test_exc_065_01/
+
+ibrun x86_64/special -mpi ./jobscripts/ca1_centerlfp_test_exc_065_01_run.hoc
+
 cp ./jobscripts/ca1_centerlfp_test_exc_065_01* ./results/ca1_centerlfp_test_exc_065_01/
