bda85399bf61af9efef10944854b05de8c453269
Ivan Raikov <ivan.g.raikov@gmail.com>
2021-01-19 13:11:42 -0600

    typo fixes

--- jobscripts/ca1_ripplestim_1x_01.sh
+++ jobscripts/ca1_ripplestim_1x_01.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH -J ca1_ripplestim_1x_01
 #SBATCH -o ./jobscripts/ca1_ripplestim_1x_01.%j.o
 #SBATCH -n 3008
@@ -8,5 +9,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-type=BEGIN
 set -x
+
 ibrun tacc_affinity x86_64/special -mpi ./jobscripts/ca1_ripplestim_1x_01_run.hoc
+
 cp ./jobscripts/ca1_ripplestim_1x_01* ./results/ca1_ripplestim_1x_01/
