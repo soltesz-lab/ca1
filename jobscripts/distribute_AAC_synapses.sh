b510f31e9e899e8c7e2ebad292ff4fc7c50f392b
Ivan Raikov <ivan.g.raikov@gmail.com>
2021-09-14 15:57:31 -0700

    new template for AAC

--- jobscripts/distribute_AAC_synapses.sh
+++ jobscripts/distribute_AAC_synapses.sh
@@ -2,8 +2,8 @@
 mpirun.mpich -np 1 python3 ./scripts/distribute_synapse_locs.py \
              --template-path templates \
               --config=Full_Scale.yaml \
-              --populations PVBC \
-              --forest-path=./datasets/PVBC_forest.h5 \
-              --output-path=./datasets/PVBC_forest_syns_20210912.h5 \
+              --populations AAC \
+              --forest-path=./datasets/AAC_forest.h5 \
+              --output-path=./datasets/AAC_forest_syns_20210912.h5 \
               --distribution=poisson \
               --io-size=1 -v
