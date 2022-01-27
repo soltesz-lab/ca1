8e4367e211aef370cb5e8daf4f70a6d7d581e02f
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-01-27 13:39:02 -0800

    BS connections

--- jobscripts/distribute_BS_synapses.sh
+++ jobscripts/distribute_BS_synapses.sh
@@ -3,7 +3,7 @@ mpirun.mpich -np 1 python3 ./scripts/distribute_synapse_locs.py \
              --template-path templates \
               --config=Full_Scale.yaml \
               --populations BS \
-              --forest-path=./datasets/BS_forest.h5 \
-              --output-path=./datasets/BS_forest_syns_20210912.h5 \
+              --forest-path=./datasets/BS_tree.h5 \
+              --output-path=./datasets/BS_forest_syns.h5 \
               --distribution=poisson \
               --io-size=1 -v
