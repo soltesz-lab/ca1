7a3c9b41b084e688a8f02d11ec24bc1a8e44b028
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-01-27 06:29:48 -0800

    adding tentative connections from CA3 onto DPYR soma

--- jobscripts/distribute_DPYR_synapses.sh
+++ jobscripts/distribute_DPYR_synapses.sh
@@ -2,8 +2,8 @@
 mpirun.mpich -np 1 python3 ./scripts/distribute_synapse_locs.py \
              --template-path templates \
               --config=Full_Scale.yaml \
-              --populations SPYR \
-              --forest-path=./datasets/SPYR_tree.h5 \
-              --output-path=./datasets/SPYR_forest_syns.h5 \
+              --populations DPYR \
+              --forest-path=./datasets/DPYR_tree.h5 \
+              --output-path=./datasets/DPYR_forest_syns.h5 \
               --distribution=poisson \
-              --io-size=1 -v --dry-run
+              --io-size=1 -v
