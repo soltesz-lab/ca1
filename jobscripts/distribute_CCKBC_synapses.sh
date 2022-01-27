124e5e07e0d70b8c81c8d92a2b3dbe72a79d1583
Ivan Raikov <ivan.g.raikov@gmail.com>
2022-01-27 11:41:47 -0800

    added CCKBC exc connections

--- jobscripts/distribute_CCKBC_synapses.sh
+++ jobscripts/distribute_CCKBC_synapses.sh
@@ -3,7 +3,7 @@ mpirun.mpich -np 1 python3 ./scripts/distribute_synapse_locs.py \
              --template-path templates \
               --config=Full_Scale.yaml \
               --populations CCKBC \
-              --forest-path=./datasets/CCKBC_forest.h5 \
-              --output-path=./datasets/CCKBC_forest_syns_20210912.h5 \
+              --forest-path=./datasets/CCKBC_tree.h5 \
+              --output-path=./datasets/CCKBC_forest_syns.h5 \
               --distribution=poisson \
               --io-size=1 -v
